b1ef06e595d3af95b71a645677ca01a16bc0023a
BrunoBucciarati99 <146854843+BrunoBucciarati99@users.noreply.github.com>
2023-10-03 17:49:22 +0200

    Update 01.beginning.R

--- 01.beginning.R
+++ 01.beginning.R
@@ -10,3 +10,23 @@ name2
 final <- name * name2
 final
 final^2
+
+# ARRAYS (bunch of inforation all toghether)
+sophi_thesis <- c(10, 20,30,50,70) #microplastics
+#functions have parenthesis and inside them there are arguments
+
+paula <- c(100,500,600,1000,2000) #people
+plot (paula, sophi_thesis)
+
+#to add arguments in a plot:
+plot(paula, sophi_thesis , xlab = "number of people", ylab0"microplastics")
+#or
+people <- paula
+microplastics <- sophi_thesis
+
+#pch = points of charachter: figures to represent points in graphs (each of them associated to a number)
+plot(people, microplastics, pch=19, cex=2, color=blue)
+#cex is the 
+
+
+     
