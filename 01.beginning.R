09a5182d8bf480f4a844fba5b1dcbfc662f356b4
BrunoBucciarati99 <146854843+BrunoBucciarati99@users.noreply.github.com>
2023-10-03 17:04:09 +0200

    Create 01.beginning.R

---
+++ 01.beginning.R
@@ -0,0 +1,12 @@
+# here i can write anything i want, the software won't read it
+
+# R AS A CALCULATOR
+2+3
+# ASSIGN TO AN OBJECT
+name <- 2+3
+name
+name2 <- 5+3
+name2
+final <- name * name2
+final
+final^2
