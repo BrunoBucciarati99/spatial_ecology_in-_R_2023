d469148ab0033fbb5cf399bf5027e6b840cb2e2c
BrunoBucciarati99 <146854843+BrunoBucciarati99@users.noreply.github.com>
2024-01-22 19:53:26 +0100

    Update 2023.final.exam.r

--- 2023.final.exam.r
+++ 2023.final.exam.r
@@ -40,10 +40,36 @@ plot(iceland2000)
 plot(iceland2010)
 plot(iceland2019)
 
-#changes in the years of the Fraction of green Vegetation Cover
+#################################
+library(raster)
+library(ggplot2)
 
-#difference between the years to see the changes
+# Read the raster file
+fcover2000 <- raster("FCOVER2000.nc")
 
-#difference between 2000 and 2010
+# Cutting the map to define the extent of the research
+ext <- c(-24, -12, 63, 67)
+iceland2000 <- crop(fcover2000, ext)
 
-#difference between 2010 and 2019
+# Convert raster to data frame
+iceland2000_df <- as.data.frame(iceland2000, xy = TRUE)
+iceland2010_df <- as.data.frame(iceland2010, xy = TRUE)
+# Plot using ggplot2
+ggplot(iceland2010_df, aes(x = x, y = y, fill = Fraction.of.green.Vegetation.Cover.1km)) +
+  geom_tile() +
+  scale_fill_viridis(option = "plasma") +
+  ggtitle("FCOVER 2010")
+
+?scale_fill_viridis
+
+#changes in the years of the Fraction of green Vegetation Cover (3-plots comparison)
+
+#difference between the years to see the changes (pixel difference)
+
+#1-->difference between 2000 and 2010 (pixel difference)
+
+#2-->difference between 2010 and 2019 (pixel difference)
+
+#3-->overall pixel difference between 2000 and 2019
+
+#data interpretation
